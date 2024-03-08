a94109dde41fc3683bbc480db70651af0d93b0d7
Rosotiana <rstcdan8@gmail.com>
2024-03-08 08:11:21 +0100

    auth

--- TacheMVC/Models/DBconnection.cs
+++ TacheMVC/Models/DBconnection.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +11,27 @@ namespace TacheMVC.Models
     {
         public static NpgsqlConnection connectionString = new NpgsqlConnection
              (ConfigurationManager.ConnectionStrings["DBConnexion"].ConnectionString);
+
+
+        public static bool Logutilisateur(Utilisateur utilisateur)
+        {
+            var req = $"SELECT * FROM public.\"Utilisateur\" WHERE name = '{utilisateur.Name}' AND \"password\"='{utilisateur.Password}';";
+            var hasUser = false;
+
+            try
+            {
+                connectionString.Open();
+                var cmd = new NpgsqlCommand(req, connectionString);
+                var reader = cmd.ExecuteReader();
+                hasUser = reader.HasRows;
+                connectionString.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return hasUser;
+        }
     }
 }
